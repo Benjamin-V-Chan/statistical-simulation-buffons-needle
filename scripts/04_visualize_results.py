3fa50b8a74b0d20233bb36acf62ee103c3704e53
BenjaminChan <benjamin.v.chan@gmail.com>
2025-03-08 22:03:39 -0800

    04_visualize_results script outline

---
+++ scripts/04_visualize_results.py
@@ -0,0 +1,6 @@
+# Import required libraries (matplotlib, seaborn)
+# Load simulation results
+# Generate a visual plot of needle drops and crossings
+# Create a histogram of estimated π values
+# Plot how the estimate converges over multiple trials
+# Save plots in 'outputs/' folder
